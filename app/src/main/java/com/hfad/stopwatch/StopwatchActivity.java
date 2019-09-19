876c910d390d7e91e599867e1d15ef2f697c7032
DrahhDev <drahh@yandex.ru>
2019-09-19 17:36:15 +0300

    button created

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -3,6 +3,7 @@ package com.hfad.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+    }
+
+    public void onClickStop(View view) {
+    }
+
+    public void onClickReset(View view) {
     }
 }
