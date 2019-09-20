efc17c376524bc58a83fe1f54ce59f66d7375fa6
DrahhDev <drahh@yandex.ru>
2019-09-20 17:49:23 +0300

    error

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -3,9 +3,16 @@ package com.hfad.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +21,39 @@ public class StopwatchActivity extends AppCompatActivity {
     }
 
     public void onClickStart(View view) {
+        running = true;
     }
 
     public void onClickStop(View view) {
+        running = false;
     }
 
     public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
     }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable(){
+            @Override
+            public void Run() {
+                int hours = seconds / 3600;
+                int minutes = seconds % 60 / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(),"%d, %02d, %02d", hours, minutes, secs);
+
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+    });
+    }
+
+
+
 }
